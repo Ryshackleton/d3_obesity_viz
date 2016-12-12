a8ef9ff4013229971ba8f839c355dbc156164e3d
Ryan Shackleton <ryan.shackleton@gmail.com>
2016-12-12 06:34:37 -0800

    Color countries by mean obesity/overweight prevalence

--- public/js/index.js
+++ public/js/index.js
@@ -14,12 +14,12 @@ for(var i=1990;i<2014;i++) {
   yearSelect.add(new Option(i));
 }
 yearSelect.selectedIndex = yearSelect.options.length-1;
-addEventListeners(yearSelect);
+yearSelect.addEventListener("change", initBarsAndMaps);
 
 /* build selector with obesity vs overweight-ness  */
 obeseSelect.add(new Option("obese"));
 obeseSelect.add(new Option("overweight"));
-addEventListeners(obeseSelect);
+obeseSelect.addEventListener("change", initBarsAndMaps);
 
 /* initialize the bar chart and map */
 initBarsAndMaps();
@@ -29,7 +29,7 @@ window.addEventListener("resize", initBarsAndMaps );
 /* add a listener for the countryclick event, which is dispatched
  * when a country is clicked on in the map */
 window.addEventListener("countryclick", function(countryclick){
-    updateCountrySelect(countryclick.detail.countryname);
+    updateCountrySelect(countryclick.detail.countrycode);
     barChart.graphUpdate(countryclick.detail);
 });
 
@@ -82,18 +82,19 @@ function advanceYear() {
   else {
     yearSelect.selectedIndex = 0;
   }
-  updateBarGraph();
+  initBarsAndMaps();
   animate = setTimeout(advanceYear, 1500);
 }
 
 /* sets up the bars and maps based on the 'page-content' width */
 function initBarsAndMaps() {
+
   /** initialize world map
-    * then link the bar chart update to the 
-    * map's click functionality
-    */
+   * then link the bar chart update to the
+   * map's click functionality
+   */
   var bodyWidth = document.getElementById('page-content').offsetWidth;
-  worldMap.init(".worldmap", bodyWidth, bodyWidth / 3 );
+  worldMap.init(".worldmap", bodyWidth, bodyWidth / 3, atts() );
 
   /* build selector with country list */
   d3.csv(locationsCSVFile, function(error,data) {
@@ -101,17 +102,20 @@ function initBarsAndMaps() {
       console.error("ui.js - Problem reading locations.csv file");
     }
     else {
-      /* soverign countries have 3 letter country codes
+      /* sovereign countries have 3 letter country codes
        * as opposed to regions, which have numbers or < 3 letters
        *  so omit the regions from our selector
       */
       var strict3LetterCountryCode = /[A-Z]{3}/;
       data.forEach(function(d) {
         if( strict3LetterCountryCode.test(d.location) ) {
-          countrySelect.add(new Option(d.location_name));
+          var newOpt = new Option();
+          newOpt.value = d.location;
+          newOpt.text = d.location_name;
+          countrySelect.add(newOpt);
         }
       });
-      addEventListeners(countrySelect);
+      countrySelect.addEventListener("change",updateBarGraph);
 
       barChart.sizeUpdate({width:bodyWidth, height:bodyWidth/2, barMargin: 2});
       barChart.init('.obesityBar', obesityCSVFileRoot, atts());
@@ -128,7 +132,8 @@ function updateBarGraph()
 /* returns an object with the currently selected attribute values */
 function atts(){
   return {
-    countryname: countrySelect.value,
+    countrycode: countrySelect.value,
+    countryname: countrySelect.text,
     sex: "both",
     obese_overweight: obeseSelect.value,
     year: yearSelect.value
@@ -136,27 +141,14 @@ function atts(){
 }
 
 /* sets the countrySelect to the specified country name */
-function updateCountrySelect(newCountryName)
+function updateCountrySelect(newCountryCode)
 {
   var ops = countrySelect.options;
   for(var i=0;i<ops.length;i++){
-    if(ops[i].text === newCountryName) {
+    if(ops[i].value === newCountryCode) {
       countrySelect.selectedIndex = i;
       break;
     }
-  }
-}
-
-/* add event listeners safely to the selectors */
-function addEventListeners(myListener) {
-  if(myListener.addEventListener) {
-      myListener.addEventListener("change", updateBarGraph);
-  }
-  else if(myListener.attachEvent) {
-      myListener.attachEvent("onchange", updateBarGraph);
-  }
-  else {
-      myListener.onchange = updateBarGraph; 
   }
 }
 
--- public/js/map.js
+++ public/js/map.js
@@ -14,7 +14,7 @@ MAP.worldClickable = function() {
   /***** PUBLIC ******/
   var mMapDivTag = ".worldmap"
 
-  var init = function(divTag,width,height) {
+  var init = function(divTag,width,height,selectedAtts) {
 
     mMapDivTag = divTag;
     mWidth = width;
@@ -33,7 +33,7 @@ MAP.worldClickable = function() {
 
     var path = d3.geo.path()
         .projection(projection);
-    
+
     /* mouseover functionality - modified from:
      (https://gist.github.com/dnprock/bb5a48a004949c7c8c60) */
     var graticule = d3.geo.graticule();
@@ -44,95 +44,171 @@ MAP.worldClickable = function() {
         .attr("d", path);
 
     svg.append("path")
-     .datum(graticule)
-     .attr("class", "choropleth")
-     .attr("d", path);
+        .datum(graticule)
+        .attr("class", "choropleth")
+        .attr("d", path);
     /* end mouseover functionality */
 
     /* for zooming functionality */
-    if( mMapSelection ) {
+    if (mMapSelection) {
       mMapSelection.selectAll("g").remove();
     }
-    else
-    {
-      mMapSelection  = svg.append("g"); 
+    else {
+      mMapSelection = svg.append("g");
     }
-    
-    d3.json("/data/countries.topo.notrans.json", function(error, topology) {
-      if( error ) {
+
+    d3.json("/data/countries.topo.notrans.json", function (error, topology) {
+      if (error) {
         console.error("map.js MAP.worldClickable.init() - Problem reading countries json file.");
         return;
       }
+      /* mapping function for colormapper, uses colorbrewer */
+      var colors = colorbrewer.Greens[9]
+          .map(function (rgb) {
+            return d3.hsl(rgb);
+          });
 
-      mMapSelection.selectAll("path")
-          .data(topojson.feature(topology, topology.objects.countries).features)
-        .enter()
-          .append("path")
-          .attr("id", "countries")
-          .attr("d", path)
-          .attr("class", "active")
-          /* use click event to trigger external updates */
-          .on("click", function(d) {
-            var countryClickEvent = new CustomEvent("countryclick", {
+      countryMeans = {};
+      d3.csv("/data/IHME_GBD_COUNTRY_MEANS.CSV", function (d) {
+        if (d.sex === selectedAtts.sex && d.metric === selectedAtts.obese_overweight
+            && d.year === selectedAtts.year) {
+          countryMeans[d.location] = +d.mean;
+          return {
+            "countrycode": d.location,
+            "countryname": d.location_name,
+            "mean": +d.mean
+          };
+        }
+      }, function (error, meanData) {
+        if (error) {
+          console.error("BARCHART.obesity.graphUpdate(); - Problem reading csv file. Check file path.");
+          return;
+        }
+
+        var md = d3.max(meanData, function (d) {
+          return d.mean;
+          });
+
+        var colors = colorbrewer.Blues[9]
+          .map(function(rgb) { return d3.hsl(rgb); });
+
+        var md = d3.max(meanData, function(d) { return d.mean; });
+        var color = d3.scale.linear()
+          .range(colors)
+          // adding a 2 here at the end of the scale for "no data" values
+          .domain([0.0, md*0.1, md*0.2, md*0.3, md*0.4, md*0.5, md*0.6, md*0.7,md*0.8, 100.0]);
+
+        /* legend help from: http://zeroviscosity.com/d3-js-step-by-step/step-3-adding-a-legend */
+        svg.selectAll('.legend').remove(); // remove any existing legend
+
+        var legendRectSize = mHeight * 0.8 / color.domain().length;
+        var legendSpacing = 0;
+        var legend = svg.selectAll('.legend')
+          .append('title')
+          .data(color.domain())
+          .enter()
+          .append('g')
+          .attr('class', 'legend')
+          .attr('transform', function(d, i) {
+            var height = legendRectSize + legendSpacing;
+            var offset =  height * color.domain().length / 2;
+            var horz = 0.3 * legendRectSize;
+            var vert = mHeight / 2 + i * height - offset;
+            return 'translate(' + horz + ',' + vert + ')'});
+
+        legend.append('rect')
+          .attr('width', legendRectSize)
+          .attr('height', legendRectSize)
+          .style('fill', color)
+          .style('stroke', 'black');
+
+        legend.append('text')
+          .attr('x', legendRectSize + legendSpacing)
+          .attr('y', legendRectSize - legendSpacing)
+          .text(function(d) {
+            if(d>1.0) {
+              return "no data";
+            } else {
+              return Math.floor(d * 100) + "%";
+            }})
+          .attr('transform', 'translate(5,'+(-legendRectSize/4)+')');
+        /* legend help from: http://zeroviscosity.com/d3-js-step-by-step/step-3-adding-a-legend */
+
+        mMapSelection.selectAll("path").remove();// remove existing path
+
+        mMapSelection.selectAll("path")
+            .data(topojson.feature(topology, topology.objects.countries).features)
+          .enter()
+            .append("path")
+            .attr("id", "countries")
+            .attr("d", path)
+            .attr("class", "active")
+            .attr("fill", function (d) {
+              return countryMeans.hasOwnProperty(d.properties.adm0_a3)
+                ? color(countryMeans[d.properties.adm0_a3]) : color(100);
+              })
+            /* use click event to trigger external updates */
+            .on("click", function (d) {
+              var countryClickEvent = new CustomEvent("countryclick", {
                 detail: {
+                  countrycode: d.properties.adm0_a3,
                   countryname: d.properties.name_long,
                   sex: "both",
                   obese_overweight: obeseSelect.value,
-                  year: yearSelect.value 
+                  year: yearSelect.value
                 }
               });
               window.dispatchEvent(countryClickEvent);
-          })
-          /* mouseover functionality modified from
-           (https://gist.github.com/dnprock/bb5a48a004949c7c8c60) */
-          .on("mousemove", function(d) {
-            $(this).attr("class", ".active");
-            var html = "";
+            })
+            /* mouseover functionality modified from
+             (https://gist.github.com/dnprock/bb5a48a004949c7c8c60) */
+            .on("mousemove", function (d) {
+              $(this).attr("class", ".active");
+              var html = "";
+              var mean = countryMeans.hasOwnProperty(d.properties.adm0_a3)
+                ? Math.floor(countryMeans[d.properties.adm0_a3] * 100) + "%" : 'no data';
+              html += "<div class=\"tooltip_kv\">";
+              html += "<span class=\"tooltip_key\">";
+              html += d.properties.name_long;
+              html += ": " + mean;
+              html += "</span>";
+              html += "</div>";
 
-            html += "<div class=\"tooltip_kv\">";
-            html += "<span class=\"tooltip_key\">";
-            html += d.properties.name_long;
-            html += "</span>";
-            html += "</div>";
+              $("#tooltip-container").html(html);
+              $(this).attr("fill-opacity", "0.8");
+              $("#tooltip-container").show();
 
-            $("#tooltip-container").html(html);
-            $(this).attr("fill-opacity", "0.8");
-            $("#tooltip-container").show();
+              var map_width = $('.choropleth')[0].getBoundingClientRect().width;
 
-            var map_width = $('.choropleth')[0].getBoundingClientRect().width;
-
-            if (d3.event.pageX < map_width / 2) {
-              d3.select("#tooltip-container")
-                .style("top", (d3.event.layerY + 15) + "px")
-                .style("left", (d3.event.layerX + 15) + "px");
-            } else {
-              var tooltip_width = $("#tooltip-container").width();
-              d3.select("#tooltip-container")
-                .style("top", (d3.event.layerY + 15) + "px")
-                .style("left", (d3.event.layerX - tooltip_width - 30) + "px");
-            }
-        })
-        .on("mouseout", function() {
-                $(this).attr("fill-opacity", "1.0");
-                $("#tooltip-container").hide();
-        });
+              if (d3.event.pageX < map_width / 2) {
+                d3.select("#tooltip-container")
+                    .style("top", (d3.event.layerY + 15) + "px")
+                    .style("left", (d3.event.layerX + 15) + "px");
+              } else {
+                var tooltip_width = $("#tooltip-container").width();
+                d3.select("#tooltip-container")
+                    .style("top", (d3.event.layerY + 15) + "px")
+                    .style("left", (d3.event.layerX - tooltip_width - 30) + "px");
+              }
+            })
+            .on("mouseout", function () {
+              $(this).attr("fill-opacity", "1.0");
+              $("#tooltip-container").hide();
+            });
         /* end mouseover functionality */
-
-      /* zoom and pan (from: http://www.digital-geography.com/d3-js-mapping-tutorial-1-set-initial-webmap/#.WEYRScMrJE6) */
-      var zoom = d3.behavior.zoom()
-          .on("zoom",function() {
-              mMapSelection.attr("transform","translate("+ 
-                  d3.event.translate.join(",")+")scale("+d3.event.scale+")")
-              mMapSelection.selectAll("path")  
-                  .attr("d", path.projection(projection)); 
       });
-      svg.call(zoom);
-
     });
+    /* zoom and pan (from: http://www.digital-geography.com/d3-js-mapping-tutorial-1-set-initial-webmap/#.WEYRScMrJE6) */
+    var zoom = d3.behavior.zoom()
+        .on("zoom", function () {
+          mMapSelection.attr("transform", "translate(" +
+              d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")")
+          mMapSelection.selectAll("path")
+              .attr("d", path.projection(projection));
+        });
+    svg.call(zoom);
+  }
 
-
-  };
-  
   return {
     /* variables */
     mMapDivTag: mMapDivTag,
